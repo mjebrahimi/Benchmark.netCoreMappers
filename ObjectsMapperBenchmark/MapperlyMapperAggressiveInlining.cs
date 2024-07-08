ee6e2406f617209e63e09eae611f14730da344f9
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-07-08 16:43:27 +0330

    Add "mapperly-artifacts" directory of experimental AggressivInlining support, Exclude from .gitignore

--- ObjectsMapperBenchmark/MapperlyMapperAggressiveInlining.cs
+++ ObjectsMapperBenchmark/MapperlyMapperAggressiveInlining.cs
@@ -1,13 +1,12 @@
 ﻿using ObjectsMapperBenchmark.Models;
 using Riok.Mapperly.Abstractions;
 
-namespace ObjectsMapperBenchmark
-{
-    [Mapper]
-    public static partial class MapperlyMapperAggressiveInlining
-    {
-        public static partial ClassSpotifyAlbum MapToClass(SpotifyAlbumDto spotifyAlbumDto);
+namespace ObjectsMapperBenchmark;
 
-        public static partial StructSpotifyAlbum MapToStruct(SpotifyAlbumDto spotifyAlbumDto);
-    }
+[Mapper]
+public static partial class MapperlyMapperAggressiveInlining
+{
+    public static partial ClassSpotifyAlbum MapToClass(SpotifyAlbumDto spotifyAlbumDto);
+
+    public static partial StructSpotifyAlbum MapToStruct(SpotifyAlbumDto spotifyAlbumDto);
 }
